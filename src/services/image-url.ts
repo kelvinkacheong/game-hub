406d09c2b35a99aeb33a7d75a3f34ac2d0f6c3c5
kelvinkacheong <kelvin.kacheong@gmail.com>
2024-03-27 22:07:10 +0800

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImg from "../assets/no_image_placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImg;
 
   const target = 'media/'
   const index = url.indexOf(target) + target.length;
